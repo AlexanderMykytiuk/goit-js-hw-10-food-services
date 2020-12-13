f4d0d0dc0e6e3bbb18afd867363c8cd6f7789147
Alexander Mykytiuk <alexandermykytiuk@gmail.com>
2020-12-13 12:02:13 +0200

    Налаштовував Prettier

--- src/JS/theme.js
+++ src/JS/theme.js
@@ -1,33 +1,31 @@
-
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
-}
+};
 
 const refs = {
-    bodyTheme: document.querySelector('body'),
-    btnTheme: document.querySelector('#theme-switch-toggle'),
-}
+  bodyTheme: document.querySelector('body'),
+  btnTheme: document.querySelector('#theme-switch-toggle'),
+};
 
 function bodyThemeChangeDark() {
-    if (refs.btnTheme.checked) {
-        refs.bodyTheme.classList.remove(Theme.LIGHT)
-        refs.bodyTheme.classList.add(Theme.DARK)
-        localStorage.setItem('theme', Theme.DARK)
-    } else {
-        refs.bodyTheme.classList.remove(Theme.DARK)
-        refs.bodyTheme.classList.add(Theme.LIGHT)
-        localStorage.setItem('theme', Theme.LIGHT)
-    }
+  if (refs.btnTheme.checked) {
+    refs.bodyTheme.classList.remove(Theme.LIGHT);
+    refs.bodyTheme.classList.add(Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    refs.bodyTheme.classList.remove(Theme.DARK);
+    refs.bodyTheme.classList.add(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
 }
 
-    function setInitialTheme() {
+function setInitialTheme() {
   if (localStorage.getItem('theme') === Theme.DARK) {
     refs.btnTheme.checked = true;
     refs.bodyTheme.classList.add(Theme.DARK);
   }
 }
 
-  setInitialTheme();
-  refs.btnTheme.addEventListener('change', bodyThemeChangeDark);  
-
+setInitialTheme();
+refs.btnTheme.addEventListener('change', bodyThemeChangeDark);
